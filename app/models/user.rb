a12d256db09b7ac5bb5fb3083846b132d45a12cc
aikawa <stbh1028@gmail.com>
2015-09-18 15:47:21 +0900

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts
 	before_save { self.email = email.downcase }
 	before_create :create_remember_token
 	validates :name, presence: true, length: { maximum: 50 }
@@ -7,13 +8,19 @@ class User < ActiveRecord::Base
     has_secure_password
     validates :password, length: { minimum: 6 }
 
-    def User.new_remember_token
-      SecureRandom.urlsafe_base64
-    end
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
 
-    def User.encrypt(token)
-      Digest::SHA1.hexdigest(token.to_s)
-    end
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  def feed
+    # このコードは準備段階です。
+    # 完全な実装は第11章「ユーザーをフォローする」を参照してください。
+    Micropost.where("user_id = ?", id)
+  end
 
     private
 
